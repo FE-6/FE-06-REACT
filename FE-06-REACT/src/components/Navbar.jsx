f781536df153967921e8cb101383f61e6c535c84
username_SyifaAulia0 <email_syifaauliafitriani7@gmail.com>
2022-11-16 00:09:41 +0700

    S-SYifa

--- FE-06-REACT/src/components/Navbar.jsx
+++ FE-06-REACT/src/components/Navbar.jsx
@@ -1,34 +1,28 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Logo from '../logo.png';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">FE 06</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                {/* <a className="nav-link active" aria-current="page" href="/">Home</a> */}
-                                <Link to={"/"}>Home </Link>
-                            </li>
-                            <li className="nav-item">
-                                {/* <a className="nav-link" href="/about">About</a> */}
-                                <Link to={"/about"}>About</Link>
-                            </li>
-                        </ul> 
-                        <ul class="nav justify-content-end">
-                            <li class="nav-item">
-                              {/* <a className="nav-link" href="/login">Login</a> */}
-                              <Link to={"/login"}>Login</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+    <>
+   
+   <nav className="navbar navbar-expand-lg bg-primary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="home.html">
+      <img src={Logo} alt="logonews" width="60" height="30"/>
+      </a>
+
+      <h3 className="nav justify-item-center text-light">AYF</h3>
+
+      <ul className="nav justify-content-end">
+        <li className="nav-item">
+          <a className="nav-link text-bg-primary p-3" href="/login">Login
+          </a>
+        </li>
+      </ul>
+    </div>      
+  </nav>
+  </>
   )
 }
 
