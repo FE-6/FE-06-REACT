878b224f541cc6aaee3f83b6ce5f425222838085
BangHann <hibatullahfarhan4@gmail.com>
2022-11-16 23:06:16 +0700

    f-paran

--- FE-06-REACT/src/components/Navbar.jsx
+++ FE-06-REACT/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import styles from "../styles/NavbarStyles.css";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+    <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">FE 06</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
