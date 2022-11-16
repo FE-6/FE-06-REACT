8005b384bf6f9e3102b265861c0d9480f052f883
BangHann <hibatullahfarhan4@gmail.com>
2022-11-16 23:17:47 +0700

    f-paran

--- FE-06-REACT/src/pages/Login.jsx
+++ FE-06-REACT/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Login from "../components/LoginData";
 
-const Project = () => {
+const Login = () => {
     return (
       <div>
         <Navbar />
@@ -12,4 +12,4 @@ const Project = () => {
     )
   }
   
-  export default Project
+  export default Login
