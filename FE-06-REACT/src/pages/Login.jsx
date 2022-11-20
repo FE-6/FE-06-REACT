02ef5d4e231728c623d72f807358ec068ca31694
BangHann <hibatullahfarhan4@gmail.com>
2022-11-20 15:50:08 +0700

    f-paran

--- FE-06-REACT/src/pages/Login.jsx
+++ FE-06-REACT/src/pages/Login.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import LoginData from "../components/LoginData";
 
-import Navbar from "../components/Navbar";
-import Login from "../components/LoginData";
-
 const Login = () => {
     return (
       <div>
